70421083a3fb7462bab72ba028ec0c15e453f73c
dyigitpolat <dyigit96@gmail.com>
2024-02-19 17:14:16 +0800

    wandb support

--- setup.py
+++ setup.py
@@ -9,3 +9,4 @@ cuda_version = int(cuda_version_string.split(".")[0]) * 10 + int(cuda_version_st
 # Create a virtual environment and install torch and torchvision
 subprocess.run(["python3", "-m", "venv", "env"]) 
 subprocess.run(["env/bin/pip", "install", "torch", "torchvision",  "--index-url" , f"https://download.pytorch.org/whl/cu{cuda_version}"])
+subprocess.run(["env/bin/pip", "install", "wandb"])
